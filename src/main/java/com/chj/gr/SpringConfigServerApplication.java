a63c53264a9f1b89fe337d09f169f0cb7773bd5c
JihedGitlab <chaaben.jihed.gitlab@gmail.com>
2025-05-05 17:37:43 +0200

    ..

--- src/main/java/com/chj/gr/SpringConfigServerApplication.java
+++ src/main/java/com/chj/gr/SpringConfigServerApplication.java
@@ -13,14 +13,18 @@ public class SpringConfigServerApplication {
 	}
 	
 	/**
-	     http://localhost:8888/z-springboot-bar-service/default
-	     http://localhost:8888/z-springboot-bar-service/dev
-    	 http://localhost:8888/z-springboot-bar-service/prod
+	     http://localhost:8762/z-springboot-bar-service/default
+	     http://localhost:8762/z-springboot-bar-service/local
+	     http://localhost:8762/z-springboot-bar-service/dev
+	     http://localhost:8762/z-springboot-bar-service/homol
+    	 http://localhost:8762/z-springboot-bar-service/prod
 	 */
 	/**
-    	https://localhost:8443/z-springboot-bar-service/default
-    	https://localhost:8443/z-springboot-bar-service/dev
-	 	https://localhost:8443/z-springboot-bar-service/prod
+    	 http://localhost:8443/z-springboot-bar-service/default
+	     http://localhost:8443/z-springboot-bar-service/local
+	     http://localhost:8443/z-springboot-bar-service/dev
+	     http://localhost:8443/z-springboot-bar-service/homol
+    	 http://localhost:8443/z-springboot-bar-service/prod
 	 */
 	
 	
